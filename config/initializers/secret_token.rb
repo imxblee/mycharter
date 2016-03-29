04590d209b05093e14376f2dd9f8947c875f535a
imxblee <transcendencez@gmail.com>
2016-03-29 08:23:01 +0000

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Mycharter::Application.config.secret_key_base = '8ed12c23abcd862a573daa226753adf46bdc5adcd08f98dafc2ee91aab8a66ea2108919167501ff10dcab64132b9aa5da2dfda50f715c35f5966b77d071b9971'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Mycharter::Application.config.session_store :cookie_store, key: '_mycharter_session'
